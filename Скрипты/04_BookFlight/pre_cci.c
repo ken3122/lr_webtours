a1385802299d9b140dde9c582765b9b14f9b751b
ken3122 <morozoffserg@gmail.com>
2020-09-28 03:25:32 +0300

    Добавлены файлы итогового ДЗ

--- Скрипты/04_BookFlight/pre_cci.c
+++ Скрипты/04_BookFlight/pre_cci.c
@@ -1,4 +1,4 @@
-# 1 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c"
+# 1 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c"
 # 1 "C:\\Program Files (x86)\\HPE\\LoadRunner\\include/lrun.h" 1
  
  
@@ -962,7 +962,7 @@ int lr_db_getvalue(char * pFirstArg, ...);
 
 
 
-# 1 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c" 2
+# 1 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c" 2
 
 # 1 "C:\\Program Files (x86)\\HPE\\LoadRunner\\include/SharedParameter.h" 1
 
@@ -1126,7 +1126,7 @@ extern VTCERR2  lrvtc_noop();
 
 
 
-# 2 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c" 2
+# 2 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c" 2
 
 # 1 "globals.h" 1
 
@@ -2583,14 +2583,14 @@ void
  
 
 
-# 3 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c" 2
+# 3 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c" 2
 
 # 1 "vuser_init.c" 1
 vuser_init()
 {
 	return 0;
 }
-# 4 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c" 2
+# 4 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c" 2
 
 # 1 "Action.c" 1
 Action()
@@ -2882,12 +2882,12 @@ Action()
 
 	return 0;
 }
-# 5 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c" 2
+# 5 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c" 2
 
 # 1 "vuser_end.c" 1
 vuser_end()
 {
 	return 0;
 }
-# 6 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\vugen\\scripts\\lr_webtours_scripts\\04_bookflight\\\\combined_04_BookFlight.c" 2
+# 6 "c:\\users\\\340\355\340\361\362\340\361\350\377\\documents\\github\\lr_webtours\\\361\352\360\350\357\362\373\\04_bookflight\\\\combined_04_BookFlight.c" 2
 
